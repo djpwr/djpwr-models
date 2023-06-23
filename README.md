64fe955ed475a0e14c57f08ae0b089ec4e79a7e5
Ruud de Klerk <ruud@codeveloped.nl>
2023-06-23 12:47:33 +0200

    Added license, readme and setup.py

---
+++ README.md
@@ -0,0 +1,1 @@
+DJPWR-Models - Model, Manager and Query related improvements for the Django web framework.
---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="djpwr-models",
+    version="0.0.1",
+    description="DJPWR - Models",
+    license='MIT',
+    author="Ruud de Klerk",
+    author_email="ruud@codeveloped.nl",
+    url='https://github.com/djpwr/djpwr-models',
+    packages=find_packages()
+)
+
