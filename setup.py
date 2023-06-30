07f4f5437eaca1ac5656cee87f3099d1caa0e4bc
Ruud de Klerk <ruud@codeveloped.nl>
2023-06-30 09:53:32 +0200

    Bumped version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="djpwr-models",
-    version="0.0.1",
+    version="0.0.2",
     description="DJPWR - Models",
     license='MIT',
     author="Ruud de Klerk",
@@ -10,4 +10,3 @@ setup(
     url='https://github.com/djpwr/djpwr-models',
     packages=find_packages()
 )
-
